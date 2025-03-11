f6ca7083042c2828c69d39ae35907e07adc2725d
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-11 16:50:58 +0900

    Creating all the functions that I will need

--- bigO_notation.py
+++ bigO_notation.py
@@ -1,3 +1,26 @@
 # Big-O Notation
 # A mathematical way of looking at which function is limiting another function
+import time
+import matplotlib.pyplot as plt 
+import numpy as np 
 
+def constant_time(n):
+    pass
+
+def logarithmic_time(n):
+    pass
+
+def linear_time(n):
+    pass
+
+def nlogn_time(n):
+    pass
+
+def quadratic_time(n):
+    pass
+
+def exponential_time(n):
+    pass
+
+def measure_time(func, n):
+    pass
