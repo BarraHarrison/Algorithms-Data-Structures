0ded2b8581eee0381d4661af49a7da921fefedd7
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-11 17:57:23 +0900

    Implementing the Logic for each function

--- omega_notation.py
+++ omega_notation.py
@@ -1,5 +1,5 @@
 # Omega Notation
-# Guarantees an algorithm will taken a certain amount of time
+# Guarantees an algorithm will take a certain amount of time
 # Basically the "best-case scenario" for each algorithm
 # Eg. Searching through an unsorted list will take a certain amount of time
 
@@ -8,20 +8,37 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 def best_case_constant(n):
-    pass
+    return 1
 
 def best_case_logarithmic(n):
-    pass
+    count = 0
+    i = 1
+    while i < n:
+        i *= 2
+        count += 1
+    return count
 
 def best_case_linear(n):
-    pass
+    return 1
 
 def best_case_nlogn(n):
-    pass
+    count = 0
+    for i in range(n):
+        j = 1
+        while j < n:
+            j *= 2
+            count += 1
+    return count
 
 def best_case_quadratic(n):
-    pass
+    count = 0
+    for i in range(n):
+        for j in range(i):
+            count += 1
+    return count
 
-def measure_time(n):
-    pass
+def measure_time(func, n):
+    start = time.time()
+    func(n)
+    return time.time() - start
 
