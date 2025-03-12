f7ec9f2a926eafa19a00e79d3d8ccd92eafa8382
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-12 20:35:15 +0900

    Trying to better understand how linked lists work

--- data_structures/linked_lists.py
+++ data_structures/linked_lists.py
@@ -1,3 +1,55 @@
 # Linked-List: Data structure made up by a series of nodes
 # Linked-Lists have dynamic-sizing which gives them an advantage over arrays
-# One-Pointer
+# One-Pointer to the next node
+
+class Node:
+    """A single node of a linked list."""
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    """Implementation of a linked list."""
+    def __init__(self):
+        self.head = None
+
+    def insert_at_end(self, data):
+        """Insert a new node at the end of the list."""
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+
+    def delete_node(self, key):
+        """Delete a node by its value."""
+        temp = self.head
+
+        if temp and temp.data == key:
+            self.head = temp.next
+            temp = None
+            return
+        
+        prev = None
+        while temp and temp.data != key:
+            prev = temp
+            temp = temp.next
+
+        if temp is None:
+            return
+        
+        prev.next = temp.next 
+        temp = None
+
+    def display(self):
+        """Print all nodes on the list."""
+        temp = self.head
+        while temp:
+            print(temp.data, end=" → ")
+            temp = temp.next
+        print("None")
+
+
