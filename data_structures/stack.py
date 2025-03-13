8bf5fc8f766a47e2099e66d8598767be82724e46
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-13 17:18:24 +0900

    Learning about Data Structure Stacks

---
+++ data_structures/stack.py
@@ -0,0 +1,5 @@
+# Stack: Linear Data Structure that follows the LIFO principle
+# Stack-Overflow: When a stack exceeds its memory limit
+# Implementing a stack using the collections.deque module
+
+from collections import deque
