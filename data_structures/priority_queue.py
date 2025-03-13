611b2e370f59512c7ce76c92e2bad0e5ae6c136f
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-13 18:08:22 +0900

    Looking at Queues and Priority-Queues

---
+++ data_structures/priority_queue.py
@@ -0,0 +1,2 @@
+# Priority-Queue: Type of Queue where elements are removed based on priority instead of arrival order
+# Basically the element with highest priority is removed first
---
+++ data_structures/queue.py
@@ -0,0 +1,3 @@
+# Queue: Linear Data Structure but follows the FIFO principle
+# Similar to Stacks but it adds and removes elements in a different way
+
