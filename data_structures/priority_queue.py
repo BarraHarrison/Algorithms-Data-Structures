7324a1ff8f26234c8b38dbe868c0d55e89076915
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-13 18:16:12 +0900

    Task A has the highest priority so it is dequeued first

--- data_structures/priority_queue.py
+++ data_structures/priority_queue.py
@@ -1,2 +1,16 @@
 # Priority-Queue: Type of Queue where elements are removed based on priority instead of arrival order
 # Basically the element with highest priority is removed first
+# heapq module provides lowest-value highest-priority
+
+import heapq
+
+priority_queue = []
+
+heapq.heappush(priority_queue, (3, "Task C"))
+heapq.heappush(priority_queue, (1, "Task A"))
+heapq.heappush(priority_queue, (2, "Task B"))
+
+print("Priority Queue:", priority_queue)
+
+print("Dequeued:", heapq.heappop(priority_queue))
+print("Priority Queue after pop:", priority_queue)
