27d74915cb5c3cccc7ca5e0f0020d60d7cfc5bcb
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-13 16:28:21 +0900

    Creating the DisjointSet class

--- algorithm_types/kruskals_algorithm.py
+++ algorithm_types/kruskals_algorithm.py
@@ -1,1 +1,30 @@
-# Algorithm for finding maximum and minimum spending trees in weighted graphs
+# Algorithm for finding maximum and minimum spanning trees (MST) in weighted graphs
+# Ensuring no cycles, it finds min. total weight that connects all the nodes in the graph
+
+class DisjointSet:
+    def __init__(self, nodes):
+        self.parent = {node: node for node in nodes}
+        self.rank = {node: 0 for node in nodes}
+
+    def find(self, node):
+        if self.parent[node] != node:
+            self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+
+    def union(self, node1, node2):
+        root1 = self.find(node1)
+        root2 = self.find(node2)
+
+        if root1 != root2:
+            if self.rank[root1] > self.rank[root2]:
+                self.parent[root2] = root1
+            elif self.rank[root1] < self.rank[root2]:
+                self.parent[root1] = root2
+            else:
+                self.parent[root2] = root1
+                self.rank[root1] += 1
+
+
+def kruskal_algorithm(graph):
+    pass
+
