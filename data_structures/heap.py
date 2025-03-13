e34ca7ab9735c31f59ae0fcdfd093ebe8384eabe
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-13 19:30:19 +0900

    Functions for the MinHeap Class

--- data_structures/heap.py
+++ data_structures/heap.py
@@ -4,3 +4,22 @@
 
 # Heapify-Up: New insert placed at the bottom of the tree, works its way "up"
 # Heapify-Down: Replace the root with the last element
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def heapify_up(self, index):
+        pass
+
+    def heapify_down(self, index):
+        pass
+
+    def push(self, value):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
