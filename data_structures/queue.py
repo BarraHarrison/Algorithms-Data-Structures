f5c99fe2db6926020be43e2f2766a037fc237af6
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-13 18:12:09 +0900

    Example of a Queue Data Structure

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -1,3 +1,18 @@
 # Queue: Linear Data Structure but follows the FIFO principle
 # Similar to Stacks but it adds and removes elements in a different way
 
+from collections import deque
+
+queue = deque()
+
+queue.append("A")
+queue.append("B")
+queue.append("C")
+
+print("Queue after enqueue:", queue)
+print("Dequeued:", queue.popleft())
+print("Queue after dequeue:", queue)
+
+# Queue after enqueue: deque(['A', 'B', 'C'])
+# Dequeued: A
+# Queue after dequeue: deque(['B', 'C'])
