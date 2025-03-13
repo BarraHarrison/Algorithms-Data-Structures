7bff5c023ee2cb7e9c4c0f4a7bd9f57c206fad15
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-13 19:25:39 +0900

    Heap in terms of priority-queues data structures

---
+++ data_structures/heap.py
@@ -0,0 +1,6 @@
+# Heap: Binary-Tree based data structure used to maintain priority-queues
+# Min-Heap: Smallest value is at the root
+# Max-Heap: Largest value is at the root
+
+# Heapify-Up: New insert placed at the bottom of the tree, works its way "up"
+# Heapify-Down: Replace the root with the last element
