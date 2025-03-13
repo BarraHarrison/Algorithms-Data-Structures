b4194b0ec025b36416be75485f9b7c093f545fb0
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-13 17:21:38 +0900

    Stack follows the last-in first-out principle

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -3,3 +3,14 @@
 # Implementing a stack using the collections.deque module
 
 from collections import deque
+
+stack = deque()
+
+stack.append("A")
+stack.append("B")
+stack.append("C")
+
+print("Stack after pushes:", stack)
+
+print("Popped:", stack.pop())
+print("Stack after pop:", stack)
