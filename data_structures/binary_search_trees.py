995f78d63a19b2c2de192e0f5db7d14b426bf275
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-14 21:42:03 +0900

    Logic for the BST functions

--- data_structures/binary_search_trees.py
+++ data_structures/binary_search_trees.py
@@ -10,24 +10,50 @@ class Node:
 
 class BST:
     def __init__(self):
-        pass
+        self.root = None
 
     def insert(self, value):
-        pass
+        """Inserting a new value into the BST"""
+        if not self.root:
+            self.root = Node(value)
+        else:
+            self._insert_recursive(self.root, value)
 
-    def insert_recursive():
-        pass
+    def _insert_recursive(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                self._insert_recursive(node.left, value)
+        else:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                self._insert_recursive(node.right, value)
 
-    def search():
-        pass
+    def search(self, value):
+        """Searching for a value in the BST"""
+        return self._search_recursive(self.root, value)
 
-    def search_recursive():
-        pass
+    def _search_recursive(self, node, value):
+        if node is None:
+            return False
+        if node.value == value:
+            return True
+        elif value < node.value:
+            return self._search_recursive(node.left, value)
+        else:
+            return self._search_recursive(node.right, value)
 
-    def inorder_traversal():
-        pass
+    def inorder_traversal(self):
+        """Returns the elements in sorted order"""
+        result = []
+        self._inorder_recursive(self.root, result)
+        return result
 
-    def _inorder_recursive():
-        pass
+    def _inorder_recursive(self, node, result):
+        if node:
+            self._inorder_recursive(node.left, result)
+            result.append(node.value)
+            self._inorder_recursive(node.right, result)
 
-    
