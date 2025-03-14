8f40be6a92371e6a0f80eee149f42bbf950fac92
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-14 21:32:17 +0900

    Functions for the BST Class

--- data_structures/binary_search_trees.py
+++ data_structures/binary_search_trees.py
@@ -1,3 +1,33 @@
 # Binary-Search Tree (BST)
 # Left child has a value smaller than the parent node
 # Right child has a value larger than the parent node
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        pass
+
+    def insert(self, value):
+        pass
+
+    def insert_recursive():
+        pass
+
+    def search():
+        pass
+
+    def search_recursive():
+        pass
+
+    def inorder_traversal():
+        pass
+
+    def _inorder_recursive():
+        pass
+
+    
