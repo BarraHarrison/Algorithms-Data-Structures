065173d7506e58c4e4e4d7b900f6f0232a8a269c
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-15 17:14:22 +0900

    Creating the 2 Classes and the functions for the AVLTree Class

--- data_structures/AVL_trees.py
+++ data_structures/AVL_trees.py
@@ -1,3 +1,35 @@
-# AVL Tree: Type of Self-Balancing Tree
+# AVL Tree: Type of Self-Balancing Binary Search Tree
 # Self-Balancing Trees re-balance themselves in logarithmic time
-# 
+# The height differences between the subtrees is at most 1
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.height = 1
+
+class AVLTree:
+    def __init__(self):
+        pass
+
+    def get_height():
+        pass
+
+    def get_balance_factor():
+        pass
+
+    def rotate_right():
+        pass
+
+    def rotate_left():
+        pass
+
+    def insert():
+        pass
+
+    def inorder_traversal():
+        pass
+
+    def insert_value():
+        pass
