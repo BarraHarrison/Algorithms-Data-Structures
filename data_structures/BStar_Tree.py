97bffe853227431b2ac1a247b2b82067e136f927
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-16 15:49:04 +0900

    Creating the BStarTreeNode and the BStarTree

--- data_structures/BStar_Tree.py
+++ data_structures/BStar_Tree.py
@@ -1,2 +1,14 @@
 # B*-Tree is an optimized version of a B-Tree
 # Splits nodes less often and ensures a higher % of node occupancy
+
+class BStarTreeNode:
+    def __init__(self, t, leaf=False):
+        """Initialize a B*-Tree Node"""
+        self.t = t
+        self.leaf = leaf
+        self.children = []
+        self.keys = []
+
+class BStarTree:
+    def __init__(self, t):
+        pass
