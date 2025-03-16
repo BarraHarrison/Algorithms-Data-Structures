93db798478b0cbc3fb4037c2931ac7ce0ae179da
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-16 15:07:01 +0900

    Creating the classes BTreeNode and BTree

--- data_structures/BTrees.py
+++ data_structures/BTrees.py
@@ -1,3 +1,28 @@
 # B-Trees are self-balancing designed to store large amounts of data
 # Unlike BST, B-Trees can have multiple children per node
 
+class BTreeNode:
+    def __init__(self, leaf=False):
+        self.leaf = leaf
+        self.keys = []
+        self.children = []
+
+class BTree:
+    def __init__(self, t):
+        pass
+
+    def traverse():
+        pass
+
+    def search():
+        pass
+
+    def insert():
+        pass
+
+    def insert_non_full():
+        pass
+
+    def split_child():
+        pass
+
