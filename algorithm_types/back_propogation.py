7d74adf34a4dad05a978544fb6e00b74ee1d30ad
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-17 13:38:58 +0900

    Inputs and Target Outputs

--- algorithm_types/back_propogation.py
+++ algorithm_types/back_propogation.py
@@ -6,8 +6,24 @@
 
 import numpy as np 
 
-def sigmoid():
-    pass
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-def sigmoid_derivative():
-    pass
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+# Training Data
+# X is inputs and y is the target outputs
+X = np.array([
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]   
+])
+
+y = np.array([
+    [0],
+    [1],
+    [1],
+    [0]
+])
