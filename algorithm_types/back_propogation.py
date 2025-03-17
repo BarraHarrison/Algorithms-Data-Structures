83668bcf28eb0bd28b144710be45bee71b6504e9
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-17 13:34:10 +0900

    Example: How a small neural network with one hidden layer uses backpropogation

--- algorithm_types/back_propogation.py
+++ algorithm_types/back_propogation.py
@@ -1,3 +1,13 @@
 # BackPropogation Algorithm vital part of training deep neural networks
 # Reduxes error on predictions using gradient descent 
 # The neural network makes better predictions as a result
+
+# How a small neural network with one hidden layer uses backpropogation
+
+import numpy as np 
+
+def sigmoid():
+    pass
+
+def sigmoid_derivative():
+    pass
