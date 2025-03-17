d2cbd022a04f8116f0e11ef406dfa21d2c3a4779
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-17 13:29:42 +0900

    BackPropogation Algorithm which is key to training deep neural networks

---
+++ algorithm_types/back_propogation.py
@@ -0,0 +1,3 @@
+# BackPropogation Algorithm vital part of training deep neural networks
+# Reduxes error on predictions using gradient descent 
+# The neural network makes better predictions as a result
